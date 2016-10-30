50153e4d292f0cb5249403a66d3148210a139009
ChrisRackauckas <me@chrisrackauckas.com>
2016-10-30 13:58:32 -0700

    setup testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,21 @@
-using RecursiveArrayTools
+using OrdinaryDiffEq, ParameterizedFunctions,
+      DiffEqBase, RecursiveArrayTools
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+
+# Here's the problem to solve
+
+f = @ode_def_nohes LotkaVolterraTest begin
+  dx = a*x - b*x*y
+  dy = -c*y + d*x*y
+end a=>1.5 b=1.0 c=3.0 d=1.0
+
+u0 = [1.0;1.0]
+tspan = [0;10.0]
+prob = ODEProblem(f,u0)
+sol = solve(prob,tspan) # this uses most of the tools
+
+t = collect(linspace(0,10,200))
+randomized = [(sol(t[i]) + .01randn(2)) for i in 1:length(t)]
+data = vecvec_to_mat(randomized)
+@test typeof(data) <: Matrix{Float64}
