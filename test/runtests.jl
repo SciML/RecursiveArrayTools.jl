eca23ea9507bd9b629db17d2d4cbbc544b4ebd9e
ChrisRackauckas <me@chrisrackauckas.com>
2016-11-14 05:45:45 -0800

    update tests to common interface

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,9 +11,9 @@ f = @ode_def_nohes LotkaVolterraTest begin
 end a=>1.5 b=1.0 c=3.0 d=1.0
 
 u0 = [1.0;1.0]
-tspan = [0;10.0]
-prob = ODEProblem(f,u0)
-sol = solve(prob,tspan) # this uses most of the tools
+tspan = (0.0,10.0)
+prob = ODEProblem(f,u0,tspan)
+sol = solve(prob) # this uses most of the tools
 
 t = collect(linspace(0,10,200))
 randomized = [(sol(t[i]) + .01randn(2)) for i in 1:length(t)]
