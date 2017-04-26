7df5c1d22060e0c5815bdc7ac0091fc5def35e65
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-04-26 15:23:50 -0700

    export AbstractArrayofVector

--- src/RecursiveArrayTools.jl
+++ src/RecursiveArrayTools.jl
@@ -10,7 +10,7 @@ module RecursiveArrayTools
   include("vector_of_array.jl")
   include("array_partition.jl")
 
-  export VectorOfArray, vecarr_to_arr
+  export VectorOfArray, AbstractVectorOfArray, vecarr_to_arr
 
   export recursivecopy!, vecvecapply, copyat_or_push!, vecvec_to_mat, recursive_one,mean
 
