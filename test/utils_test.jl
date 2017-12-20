2396aeac59b3f3203d2820a82926230704c02bbc
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-12-20 09:13:08 -0800

    don't test upstream

--- test/utils_test.jl
+++ test/utils_test.jl
@@ -1,22 +1,9 @@
-using OrdinaryDiffEq, ParameterizedFunctions,
-      DiffEqBase, RecursiveArrayTools, Unitful, StaticArrays
+using RecursiveArrayTools, Unitful, StaticArrays
 using Base.Test
 
-# Here's the problem to solve
-
-f = @ode_def_nohes LotkaVolterraTest begin
-  dx = a*x - b*x*y
-  dy = -c*y + d*x*y
-end a=>1.5 b=1.0 c=3.0 d=1.0
-
-u0 = [1.0;1.0]
-tspan = (0.0,10.0)
-prob = ODEProblem(f,u0,tspan)
-sol = solve(prob,Tsit5()) # this uses most of the tools
-
 t = collect(linspace(0,10,200))
-randomized = [(sol(t[i]) + .01randn(2)) for i in 1:length(t)]
-data = vecvec_to_mat(randomized)
+randomized = VectorOfArray([.01randn(2) for i in 1:10])
+data = convert(Array,randomized)
 @test typeof(data) <: Matrix{Float64}
 
 ## Test means
