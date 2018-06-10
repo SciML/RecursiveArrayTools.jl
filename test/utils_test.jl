c4462c5dbe31127ccdc550ca027760b9a79e6324
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-06-10 16:16:47 -0700

    remove extra recursive_mean

--- test/utils_test.jl
+++ test/utils_test.jl
@@ -10,18 +10,6 @@ data = convert(Array,randomized)
 A = [[1 2; 3 4],[1 3;4 6],[5 6;7 8]]
 @test recursive_mean(A) ≈ [2.33333333 3.666666666
            4.6666666666 6.0]
-B = Matrix{Matrix{Int64}}(2,3)
-B[1,:] = [[1 2; 3 4],[1 3;4 6],[5 6;7 8]]
-B[2,:] = [[1 2; 3 4],[1 5;4 3],[5 8;2 1]]
-
-a = [[1 2; 3 4],[1 4; 4 4.5],[5 7; 4.5 4.5]]
-@test recursive_mean(B,1)[1] ≈ a[1]
-@test recursive_mean(B,1)[2] ≈ a[2]
-@test recursive_mean(B,1)[3] ≈ a[3]
-
-a = [[2.333333333333 4.666666666666; 3.6666666666666 6.0], [2.3333333 3.0; 5.0 2.6666666]]
-@test_broken recursive_mean(B,2)[1] ≈ a[1]
-@test_broken recursive_mean(B,2)[2] ≈ a[2]
 
 A = zeros(5,5)
 recursive_unitless_eltype(A) == Float64
