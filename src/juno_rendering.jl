5b8ca661c5b99b796c60f2deb336e21eb40a72e2
Christopher Rackauckas <accounts@chrisrackauckas.com>
2018-07-03 22:44:41 -0700

    Update juno_rendering.jl

--- src/juno_rendering.jl
+++ src/juno_rendering.jl
@@ -1,4 +1,4 @@
-@require Juno begin
+@require Juno="e5e0dc1b-0480-54bc-9374-aad01c23163d" begin
   # Juno Rendering
   Juno.render(i::Juno.Inline, x::ArrayPartition) = Juno.render(i, Juno.defaultrepr(x))
   Juno.render(i::Juno.Inline, x::AbstractVectorOfArray) = Juno.render(i, Juno.defaultrepr(x))
