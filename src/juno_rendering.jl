481352a42ded05412d0c0074adc7d210a434a472
Yingbo Ma <mayingbo5@gmail.com>
2019-07-02 18:33:17 -0400

    Move at-require to __init__

--- src/juno_rendering.jl
+++
@@ -1,5 +0,0 @@
-@require Juno="e5e0dc1b-0480-54bc-9374-aad01c23163d" begin
-  # Juno Rendering
-  Juno.render(i::Juno.Inline, x::ArrayPartition) = Juno.render(i, Juno.defaultrepr(x))
-  Juno.render(i::Juno.Inline, x::AbstractVectorOfArray) = Juno.render(i, Juno.defaultrepr(x))
-end
