84f3051388de0c0ea984f34ff94a1db454bf46a8
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-05-11 20:58:03 -0400

    hotfix

--- src/utils.jl
+++ src/utils.jl
@@ -86,7 +86,7 @@ recursive_unitless_bottom_eltype(a) = recursive_unitless_bottom_eltype(typeof(a)
 recursive_unitless_bottom_eltype(a::Type{T}) where T = recursive_unitless_bottom_eltype(eltype(a))
 recursive_unitless_bottom_eltype(a::Type{T}) where {T<:AbstractArray} = recursive_unitless_bottom_eltype(eltype(a))
 recursive_unitless_bottom_eltype(a::Type{T}) where {T<:Number} = eltype(a) == Number ? Float64 : typeof(one(eltype(a)))
-recursive_unitless_bottom_eltype(::Type{Enum{T}}) where T = T
+recursive_unitless_bottom_eltype(::Type{<:Enum{T}}) where T = T
 
 recursive_unitless_eltype(a) = recursive_unitless_eltype(eltype(a))
 recursive_unitless_eltype(a::Type{T}) where {T<:StaticArray} = similar_type(a,recursive_unitless_eltype(eltype(a)))
