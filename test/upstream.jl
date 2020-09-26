11b09886900f6398f906213e3f9962fa956b3765
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-09-26 19:37:07 -0400

    fix ArrayPartition ZeroMatrix, i.e. stiff ODEs on recursive AP

--- test/upstream.jl
+++ test/upstream.jl
@@ -28,3 +28,21 @@ F = ArrayPartition([0.0 0.0; 0.0 0.0],[0.0 0.0; 0.0 0.0])
 u0= ArrayPartition([0.1 1.2; 0.1 1.2], [0.1 1.2; 0.1 1.2])
 result = mymodel(F, u0)
 nlsolve(mymodel, u0)
+
+# Nested ArrayPartition solves
+
+dyn(u, p, t) = ArrayPartition(
+    ArrayPartition(zeros(1), [0.0]),
+    ArrayPartition(zeros(1), [0.0])
+)
+
+solve(
+    ODEProblem(
+        dyn,
+        ArrayPartition(
+            ArrayPartition(zeros(1), [-1.0]),
+            ArrayPartition(zeros(1), [0.75])
+        ),
+        (0.0, 1.0)
+    )
+)
