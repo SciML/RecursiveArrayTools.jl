cf165647dc44eb24cad4638d9f48b370ef320b94
Lucas Morton <23484003+lamorton@users.noreply.github.com>
2021-06-01 10:17:29 -0700

    Makes the two methods use the same interface. Error message when using symbolic indexing becomes more informative. Prelude to an actual fix for https://github.com/SciML/ModelingToolkit.jl/issues/972.

--- src/zygote.jl
+++ src/zygote.jl
@@ -1,6 +1,7 @@
 ZygoteRules.@adjoint function getindex(VA::AbstractVectorOfArray, i)
   function AbstractVectorOfArray_getindex_adjoint(Δ)
-    Δ′ = [ (i == j ? Δ : zero(x)) for (x,j) in zip(VA.u, 1:length(VA))]
+    Δ′ = zero(VA)
+    Δ′[i] = Δ
     (Δ′,nothing)
   end
   VA[i],AbstractVectorOfArray_getindex_adjoint
