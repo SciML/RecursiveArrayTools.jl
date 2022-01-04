8b02fa62d356a1c3d2b45922803b7d1615a959df
Amin Sadeghi <amin.sadeghi@live.com>
2022-01-03 19:56:12 -0500

    Specialize getindex for indexing VectorOfArray with boolean mask and colon

--- test/gpu/vectorofarray_gpu.jl
+++ test/gpu/vectorofarray_gpu.jl
@@ -1,6 +1,7 @@
-using RecursiveArrayTools, CUDA
+using RecursiveArrayTools, CUDA, Test
 CUDA.allowscalar(false)
 
+# Test indexing with colon
 x = zeros(5)
 y = VectorOfArray([x,x,x])
 y[:,:]
@@ -8,3 +9,18 @@ y[:,:]
 x = CUDA.zeros(5)
 y = VectorOfArray([x,x,x])
 y[:,:]
+
+# Test indexing with boolean masks and colon
+nx, ny, nt = 3, 4, 5
+x = CUDA.rand(nx, ny, nt)
+m = CUDA.rand(nx, ny) .> 0.5
+x[m, :]
+
+va = VectorOfArray([slice for slice in eachslice(x, dims=3)])
+@test va[m, :] ≈ x[m, :]
+va[m, :]
+
+xc = Array(x)
+mc = Array(m)
+xc[mc, :]
+
