b0cca18f4dcde1b408b4d10e0b9d074e57ba351d
Vedant Puri <vedantpuri@gmail.com>
2022-01-31 17:38:00 +0530

    run tests on personal machines
    no tests were running on my personal machine earlier.
    ```julia
         Testing Running tests...
      0.000008 seconds
         Testing RecursiveArrayTools tests passed 
    
    (RecursiveArrayTools) pkg> 
    ```
    
    with this change, all the "light" tests will run on personal machines

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,13 +19,14 @@ end
 
 @time begin
 
+@time @testset "Utils Tests" begin include("utils_test.jl") end
+@time @testset "Partitions Tests" begin include("partitions_test.jl") end
+@time @testset "VecOfArr Indexing Tests" begin include("basic_indexing.jl") end
+@time @testset "VecOfArr Interface Tests" begin include("interface_tests.jl") end
+@time @testset "StaticArrays Tests" begin include("copy_static_array_test.jl") end
+@time @testset "Linear Algebra Tests" begin include("linalg.jl") end
+
 if !is_APPVEYOR && GROUP == "Core"
-  @time @testset "Utils Tests" begin include("utils_test.jl") end
-  @time @testset "Partitions Tests" begin include("partitions_test.jl") end
-  @time @testset "VecOfArr Indexing Tests" begin include("basic_indexing.jl") end
-  @time @testset "VecOfArr Interface Tests" begin include("interface_tests.jl") end
-  @time @testset "StaticArrays Tests" begin include("copy_static_array_test.jl") end
-  @time @testset "Linear Algebra Tests" begin include("linalg.jl") end
   @time @testset "Upstream Tests" begin include("upstream.jl") end
   @time @testset "Adjoint Tests" begin include("adjoints.jl") end
 end
