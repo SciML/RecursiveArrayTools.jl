15df32bd7973a7eb530fd766f15297a8a8fda390
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-04-08 23:30:12 -0400

    Add recursivefill!

--- test/utils_test.jl
+++ test/utils_test.jl
@@ -2,31 +2,31 @@ using RecursiveArrayTools, StaticArrays
 using Test
 
 t = collect(range(0, stop=10, length=200))
-randomized = VectorOfArray([.01randn(2) for i in 1:10])
-data = convert(Array,randomized)
+randomized = VectorOfArray([0.01randn(2) for i in 1:10])
+data = convert(Array, randomized)
 @test typeof(data) <: Matrix{Float64}
 
 ## Test means
-A = [[1 2; 3 4],[1 3;4 6],[5 6;7 8]]
+A = [[1 2; 3 4], [1 3; 4 6], [5 6; 7 8]]
 @test recursive_mean(A) ≈ [2.33333333 3.666666666
-           4.6666666666 6.0]
+  4.6666666666 6.0]
 
-A = zeros(5,5)
+A = zeros(5, 5)
 @test recursive_unitless_eltype(A) == Float64
 
 using Unitful
-A = zeros(5,5)*1u"kg"
+A = zeros(5, 5) * 1u"kg"
 @test recursive_unitless_eltype(A) == Float64
-AA = [zeros(5,5) for i in 1:5]
+AA = [zeros(5, 5) for i in 1:5]
 @test recursive_unitless_eltype(AA) == Array{Float64,2}
 AofA = [copy(A) for i in 1:5]
 @test recursive_unitless_eltype(AofA) == Array{Float64,2}
-AofSA = [@SVector [2.0,3.0] for i in 1:5]
+AofSA = [@SVector [2.0, 3.0] for i in 1:5]
 @test recursive_unitless_eltype(AofSA) == SVector{2,Float64}
-AofuSA = [@SVector [2.0u"kg",3.0u"kg"] for i in 1:5]
+AofuSA = [@SVector [2.0u"kg", 3.0u"kg"] for i in 1:5]
 @test recursive_unitless_eltype(AofuSA) == SVector{2,Float64}
 
-A = [ArrayPartition(ones(1),ones(1)),]
+A = [ArrayPartition(ones(1), ones(1)),]
 
 function test_recursive_bottom_eltype()
   function test_value(val::Any, expected_type::Type)
@@ -70,3 +70,22 @@ test_recursive_bottom_eltype()
 using RecursiveArrayTools: issymbollike
 @test !issymbollike(1)
 @test issymbollike(:a)
+
+x = zeros(10)
+recursivefill!(x, 1.0)
+@test x == ones(10)
+
+x = [zeros(10), zeros(10)]
+recursivefill!(x, 1.0)
+@test x[1] == ones(10)
+@test x[2] == ones(10)
+
+x = [SVector{10}(zeros(10)), SVector{10}(zeros(10))]
+recursivefill!(x, SVector{10}(ones(10)))
+@test x[1] == SVector{10}(ones(10))
+@test x[2] == SVector{10}(ones(10))
+
+x = [MVector{10}(zeros(10)), MVector{10}(zeros(10))]
+recursivefill!(x, 1.0)
+@test x[1] == MVector{10}(ones(10))
+@test x[2] == MVector{10}(ones(10))
