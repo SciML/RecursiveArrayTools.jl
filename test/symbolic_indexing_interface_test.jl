69f70a7f4f698b44c1b9f3375d9e918d1067b079
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2022-12-08 16:00:23 +0530

    Fixed DiffEqArray constructors, added tests
    - DiffEqArray constructs SymbolCache taking into account if `indepsym` is
      `nothing`, a single symbol or an `AbstractArray` of symbols
    - Test symbolic indexing functionality

--- test/symbolic_indexing_interface_test.jl
+++ test/symbolic_indexing_interface_test.jl
@@ -26,3 +26,17 @@ sc = SymbolCache([:a, :b], [:t], [:c, :d])
 @test isnothing(param_sym_to_index(sc, :a))
 @test all(is_param_sym.((sc,), [:c, :d]))
 @test !is_param_sym(sc, :b)
+
+t = 0.0:0.1:1.0
+f(x) = 2x
+f2(x) = 3x
+
+dx = DiffEqArray([[f(x), f2(x)] for x in t], t, [:a, :b], :t)
+@test dx[:t] == t
+@test dx[:a] == [f(x) for x in t]
+@test dx[:b] == [f2(x) for x in t]
+
+dx = DiffEqArray([[f(x), f2(x)] for x in t], t, [:a, :b], [:t])
+@test dx[:t] == t
+dx = DiffEqArray([[f(x), f2(x)] for x in t], t, [:a, :b])
+@test_throws Exception dx[nothing] # make sure it isn't storing [nothing] as indepsym
