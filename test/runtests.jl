116942a4c1aee1362518dbb75decf9a47ae1a510
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2022-12-12 13:29:47 +0530

    Add symbolic_indexing_interface_test.jl to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,6 +23,7 @@ if GROUP == "Core" || GROUP == "All"
   @time @testset "Utils Tests" begin include("utils_test.jl") end
   @time @testset "Partitions Tests" begin include("partitions_test.jl") end
   @time @testset "VecOfArr Indexing Tests" begin include("basic_indexing.jl") end
+  @time @testset "SymbolicIndexingInterface API test" begin include("symbolic_indexing_interface_test.jl") end
   @time @testset "VecOfArr Interface Tests" begin include("interface_tests.jl") end
   @time @testset "Table traits" begin include("tabletraits.jl") end
   @time @testset "StaticArrays Tests" begin include("copy_static_array_test.jl") end
