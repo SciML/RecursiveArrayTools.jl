f294f7ad36bb49edf23a8baedfb69b62d3fd41fc
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-02-06 18:03:05 -0500

    fix requires import

--- src/RecursiveArrayTools.jl
+++ src/RecursiveArrayTools.jl
@@ -42,8 +42,8 @@ function ChainRulesCore.rrule(T::Type{<:GPUArraysCore.AbstractGPUArray},
     T(xs), ȳ -> (NoTangent(), ȳ)
 end
 
+import Requires
 @static if !isdefined(Base, :get_extension)
-    import Requires
     function __init__()
             Requires.@require Tracker="9f7883ad-71c0-57eb-9f7f-b5c9e6d3789c" begin include("../ext/RecursiveArrayToolsTrackerExt.jl") end
     end
