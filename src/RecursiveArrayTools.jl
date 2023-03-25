08f39929c88c1dc570712a610c0109af83fcc379
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-03-25 11:34:34 -0400

    Remove FillArrays dependency to reduce import times
    It was added in https://github.com/SciML/RecursiveArrayTools.jl/commit/b3ed973b0167f0e60f4b7c4e801af850ce021b17 but in https://github.com/PumasAI/DataInterpolations.jl/issues/129 it has been identified as one of the main import time contributions. It's only necessary for the Zygote rules, and not even ChainRules but specifically `Zygote.@adjoint`, so they are simply moved to a Zygote extension and use FillArrays from Zygote to achieve the same goal.

--- src/RecursiveArrayTools.jl
+++ src/RecursiveArrayTools.jl
@@ -13,8 +13,6 @@ import ChainRulesCore
 import ChainRulesCore: NoTangent
 import ZygoteRules, Adapt
 
-using FillArrays
-
 import Tables, IteratorInterfaceExtensions
 
 abstract type AbstractVectorOfArray{T, N, A} <: AbstractArray{T, N} end
@@ -41,6 +39,7 @@ import Requires
 @static if !isdefined(Base, :get_extension)
     function __init__()
         Requires.@require Tracker="9f7883ad-71c0-57eb-9f7f-b5c9e6d3789c" begin include("../ext/RecursiveArrayToolsTrackerExt.jl") end
+        Requires.@require Zygote="e88e6eb3-aa80-5325-afca-941959d7151f" begin include("../ext/RecursiveArrayToolsZygoteExt.jl") end
     end
 end
 
