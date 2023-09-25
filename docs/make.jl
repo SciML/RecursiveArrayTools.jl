f1e4530d7aae6ef0d60b797e50c2742f5fbe301a
Arno Strouwen <arno.strouwen@telenet.be>
2023-09-25 06:38:49 +0200

    Documenter 1.0 upgrade

--- docs/make.jl
+++ docs/make.jl
@@ -9,16 +9,8 @@ makedocs(sitename = "RecursiveArrayTools.jl",
          authors = "Chris Rackauckas",
          modules = [RecursiveArrayTools],
          clean = true, doctest = false, linkcheck = true,
-         strict = [
-             :doctest,
-             :linkcheck,
-             :parse_error,
-             :example_block,
-             # Other available options are
-             # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
-         ],
-         format = Documenter.HTML(analytics = "UA-90474609-3",
-                                  assets = ["assets/favicon.ico"],
+         warnonly = [:missing_docs],
+         format = Documenter.HTML(assets = ["assets/favicon.ico"],
                                   canonical = "https://docs.sciml.ai/RecursiveArrayTools/stable/"),
          pages = pages)
 
