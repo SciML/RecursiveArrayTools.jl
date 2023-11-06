2727653432ed30b6d7a6ee354b5ffd4650778102
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2023-11-06 15:25:31 +0530

    refactor: refactor tests with new constructors

--- test/symbolic_indexing_interface_test.jl
+++ test/symbolic_indexing_interface_test.jl
@@ -4,14 +4,14 @@ t = 0.0:0.1:1.0
 f(x) = 2x
 f2(x) = 3x
 
-dx = DiffEqArray([[f(x), f2(x)] for x in t], t, [:a, :b], :t)
+dx = DiffEqArray([[f(x), f2(x)] for x in t], t; variables = [:a, :b], independent_variables = [:t])
 @test dx[:t] == t
 @test dx[:a] == [f(x) for x in t]
 @test dx[:b] == [f2(x) for x in t]
 
-dx = DiffEqArray([[f(x), f2(x)] for x in t], t, [:a, :b], [:t])
+dx = DiffEqArray([[f(x), f2(x)] for x in t], t; variables = [:a, :b], independent_variables = [:t])
 @test dx[:t] == t
-dx = DiffEqArray([[f(x), f2(x)] for x in t], t, [:a, :b])
+dx = DiffEqArray([[f(x), f2(x)] for x in t], t; variables = [:a, :b])
 @test_throws Exception dx[nothing] # make sure it isn't storing [nothing] as indepsym
 
 ABC = @SLVector (:a, :b, :c);
