919df69d6e546d91f7b8157ffcc45d974a2d5d87
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2023-11-16 18:46:31 +0530

    test: mark measurements and units test as broken

--- test/downstream/measurements_and_units.jl
+++ test/downstream/measurements_and_units.jl
@@ -6,4 +6,4 @@ tspan3 = (0.0, 1.0) .* u"s"
 f3(du, u, p, t) = [0.0u"m/s^2", -g3]
 u3 = [0.0, 0.0] .* u"m"
 problem4 = SecondOrderODEProblem(f3, du4, u3, tspan3)
-solve(problem4, Tsit5())
+@test_broken solve(problem4, Tsit5())
