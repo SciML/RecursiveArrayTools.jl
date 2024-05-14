53cd1ae3de1cc788914df945b53568acd4a1655a
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-05-14 17:18:58 -0400

    hotfix scalar path

--- ext/RecursiveArrayToolsReverseDiffExt.jl
+++ ext/RecursiveArrayToolsReverseDiffExt.jl
@@ -9,8 +9,8 @@ function trackedarraycopyto!(dest, src)
     for (i, slice) in zip(eachindex(dest.u), eachslice(src, dims = ndims(src)))
         if dest.u[i] isa AbstractArray
             dest.u[i] = reshape(ArrayInterface.aos_to_soa(slice), size(dest.u[i]))
-        elseif dest.u[i]
-            trackedarraycopyto!(dest.u[i], slice)
+        elseif dest.u[i] isa Number
+            dest.u[i] = slice
         end
     end
 end
