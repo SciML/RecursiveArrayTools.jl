8e66b0ae16abdf131d4328d3a51e9abb183c73e8
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-08-10 04:57:09 -0400

    Make sparsearrays into a weak dependency

---
+++ ext/RecursiveArrayToolsSparseArraysExt.jl
@@ -0,0 +1,20 @@
+module RecursiveArrayToolsSparseArrays
+
+import SparseArrays
+import RecursiveArrayTools
+
+function Base.copyto!(dest::SparseArrays.AbstractCompressedVector, A::RecursiveArrayToolsArrayPartition)
+    @assert length(dest) == length(A)
+    cur = 1
+    @inbounds for i in 1:length(A.x)
+        if A.x[i] isa Number
+            dest[cur:(cur + length(A.x[i]) - 1)] .= A.x[i]
+        else
+            dest[cur:(cur + length(A.x[i]) - 1)] .= vec(A.x[i])
+        end
+        cur += length(A.x[i])
+    end
+    dest
+end
+
+end
