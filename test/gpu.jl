e812893306e63e8aa7965f9f01cea1b657efeac1
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-04-21 22:37:35 -0400

    Update gpu.jl

--- test/gpu.jl
+++ test/gpu.jl
@@ -1,4 +1,15 @@
-using RecursiveArrayTools, CuArrays
+using RecursiveArrayTools, Adapt, CuArrays, Test
 a = ArrayPartition(([1.0f0] |> cu, [2.0f0] |> cu, [3.0f0] |> cu))
 b = ArrayPartition(([0.0f0] |> cu, [0.0f0] |> cu, [0.0f0] |> cu))
 @. a + b
+
+a = VectorOfArray([ones(2) for i in 1:3])
+_a = Adapt.adapt(CuArray,a)
+@test _a isa VectorOfArray
+@test _a.u isa Vector{<:CuArray}
+
+b = DiffEqArray([ones(2) for i in 1:3],ones(2))
+_b = Adapt.adapt(CuArray,b)
+@test _b isa DiffEqArray
+@test _b.u isa Vector{<:CuArray}
+@test _b.t isa CuArray
