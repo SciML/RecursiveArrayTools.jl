2acf709dd2ceb73257ca2b1c5c6f102bd76b4a8e
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-12-19 09:44:32 +0100

    more JET tests to test group

---
+++ test/jet_tests.jl
@@ -0,0 +1,23 @@
+using Pkg
+Pkg.add("JET")
+Pkg.instantiate()
+using JET
+using RecursiveArrayTools
+
+# Get all reports first
+result = JET.report_package(RecursiveArrayTools; target_modules = (RecursiveArrayTools,))
+reports = JET.get_reports(result)
+
+# Filter out similar_type inference errors from StaticArraysCore
+filtered_reports = filter(reports) do report
+    s = string(report)
+    !(occursin("similar_type", s) && occursin("StaticArraysCore", s))
+end
+
+# Check if there are any non-filtered errors
+if !isempty(filtered_reports)
+    @error "JET found errors" filtered_reports
+    exit(1)
+else
+    @info "All JET errors are filtered (similar_type related)"
+end
